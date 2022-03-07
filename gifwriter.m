4a6c7cc3495cc0c8cb71ecd368ba763be7b295b9
Vivek T Ramamoorthy <vivektramamoorthy@gmail.com>
2022-03-07 11:40:27 +0100

    updated gifwriter removed unrecog chars

--- gifwriter.m
+++ gifwriter.m
@@ -18,24 +18,24 @@
 %% CHECKS
 % This section performs check 
 if ~exist('giffilename','var') || ~exist('gifdelaytime','var') ||...
-        ~exist('giftrigger','var') || ~exist('writegif','var')
-    %% GIF WRITER CONTROLS
-    giffilename = './myanimation.gif'; % Name of the gif file
-    gifdelaytime = 0.3; % delay time between each image
-    giftrigger = true; % Trigger that resets the writing from first image
-    writegif = true; % Whether or not to save gif. Toggle this off to stop writing gif
+    ~exist('giftrigger','var') || ~exist('writegif','var')
+    %% GIF WRITER CONTROLS
+    giffilename = './myanimation.gif'; % Name of the gif file
+    gifdelaytime = 0.3; % delay time between each image
+    giftrigger = true; % Trigger that resets the writing from first image
+    writegif = true; % Whether or not to save gif. Toggle this off to stop writing gif
 end
 
 %% WRITING TO GIF
 
 if writegif
-    frame = getframe(gcf);
-    im = frame2im(frame);
-    [imind,cm] = rgb2ind(im,256);
-    if giftrigger==1   % initializes the gif file
-        imwrite(imind,cm,giffilename,'gif', 'Loopcount',inf,'DelayTime',gifdelaytime);
-        giftrigger=0;
-    else % appends to existing gif file
-        imwrite(imind,cm,giffilename,'gif','WriteMode','append','DelayTime',gifdelaytime);
-    end
+    frame = getframe(gcf);
+    im = frame2im(frame);
+    [imind,cm] = rgb2ind(im,256);
+    if giftrigger==1 % initializes the gif file
+        imwrite(imind,cm,giffilename,'gif', 'Loopcount',inf,'DelayTime',gifdelaytime);
+        giftrigger=0;
+    else % appends to existing gif file
+        imwrite(imind,cm,giffilename,'gif','WriteMode','append','DelayTime',gifdelaytime);
+    end
 end
